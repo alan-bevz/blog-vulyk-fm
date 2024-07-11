076fdd0965782265fe2f7c790e7424a8266c1f32
kressnik <r.bevz@bidsbee.com>
2024-07-11 18:41:25 +0100

    add @nuxtjs/tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint'
-  ],
+  modules: ['@nuxt/eslint', "@nuxtjs/tailwindcss"],
   eslint: {
     // options here
   }
