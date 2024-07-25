766e5ed273f7ab7a65390d319a34da19b9ea11ac
alan-bevz <bevz587@gmail.com>
2024-07-25 13:06:49 +0100

    feat(layouts): added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Vulyk FM</h1>
+      <nav>
+        <NuxtLink to="/">Головна</NuxtLink>
+        <NuxtLink to="/auth/login">Авторизація</NuxtLink>
+        <NuxtLink to="/auth/register">Реєстрація</NuxtLink>
+      </nav>
+    </header>
+    <main>
+      <NuxtPage />
+    </main>
+    <footer>
+      <p>&copy; 2024 Vulyk FM</p>
+    </footer>
+  </div>
+</template>
